eafb6b48617b24db4a0706da739076b397ac233b
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2021-12-10 09:14:56 -0800

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import { fetchBooks } from './redux/books/books';
 
 ReactDOM.render(
   <React.StrictMode>
