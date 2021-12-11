d665088247ee333e704ae4506d1f5b5edf36f1c5
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2021-12-11 03:12:14 -0800

    Update the Book.js file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,39 @@
-import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { deleteBook } from '../redux/books/books';
 
 const Book = (props) => {
   const { book } = props;
   const dispatch = useDispatch();
   return (
-    <li key={book.item_id}>
-      <span>{` ${book.title} `}</span>
-      <span>{`${book.category} `}</span>
-      <button type="button" onClick={() => dispatch(deleteBook(book.item_id))}>Delete Book</button>
+    <li key={book.item_id} className="book-sec">
+      <div className="book-info">
+        <span className="book-cat">
+          {` ${book.category}`}
+        </span>
+        <span className="book-name">
+          {` ${book.title} `}
+        </span>
+      </div>
+      <div className="btn-sec">
+        <button type="button" className="btn-c"> Comments </button>
+        <div className="line-2" />
+        <button type="button" className="btn-r" onClick={() => dispatch(deleteBook(book.item_id))}>
+          Remove
+        </button>
+        <div className="line-2" />
+        <button type="button" className="btn-e"> Edit </button>
+      </div>
     </li>
   );
 };
 
-export default Book;
-
 Book.propTypes = {
   book: PropTypes.shape({
-    item_id: PropTypes.number,
+    item_id: PropTypes.string,
     title: PropTypes.string,
     category: PropTypes.string,
   }).isRequired,
 };
+
+export default Book
